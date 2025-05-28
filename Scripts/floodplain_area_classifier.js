769b6ffde210a2dfff7b23f17da9d3bf54559e49
Jessé Burlamaque <burlamaque.jess@gmail.com>
2025-05-27 23:38:06 -0300

    add training samples and classification

--- Scripts/floodplain_area_classifier.js
+++ Scripts/floodplain_area_classifier.js
@@ -89,3 +89,80 @@ var compositeSeca = compositeSecaRadar
   .addBands(ndwiSeca)
   .addBands(dem.rename('elevation'))
   .addBands(gedi);
+
+  // 5. Amostras de treino (classes 0, 1, 2)
+var agua = geometry1.map(function(feat) { return feat.set('class', 1); });
+var terra = geometry2.map(function(feat) { return feat.set('class', 0); });
+var umida = geometry3.map(function(feat) { return feat.set('class', 2); });
+var trainingSamples = agua.merge(terra).merge(umida);
+
+// 6. Treinamento com todas as variáveis
+var training = compositeCheia.sampleRegions({
+  collection: trainingSamples,
+  properties: ['class'],
+  scale: 60
+});
+
+var classifier = ee.Classifier.smileRandomForest(50).train({
+  features: training,
+  classProperty: 'class',
+  inputProperties: ['VH', 'B2', 'B3', 'B4', 'B8', 'NDWI', 'elevation', 'canopy_height']
+});
+
+// 7. Classificação cheia e seca
+var classifiedCheia = compositeCheia.classify(classifier).clip(aoi);
+var classifiedSeca = compositeSeca.classify(classifier).clip(aoi);
+
+// 8. Suavização
+var smoothedCheia = classifiedCheia.focal_mode(1.5, 'circle', 'pixels');
+var smoothedSeca = classifiedSeca.focal_mode(1.5, 'circle', 'pixels');
+
+// 9. Polígonos vetoriais (cheia)
+var waterVector = smoothedCheia.eq(1).selfMask().reduceToVectors({geometry: aoi, scale: 60, geometryType: 'polygon'});
+var landVector = smoothedCheia.eq(0).selfMask().reduceToVectors({geometry: aoi, scale: 60, geometryType: 'polygon'});
+var humidVector = smoothedCheia.eq(2).selfMask().reduceToVectors({geometry: aoi, scale: 60, geometryType: 'polygon'});
+
+// 10. Cálculo de áreas (cheia)
+function calcularAreaRaster(img, nome) {
+  var area_ha = img.multiply(ee.Image.pixelArea()).divide(10000)
+    .reduceRegion({reducer: ee.Reducer.sum(), geometry: aoi, scale: 60, maxPixels: 1e13});
+  print('Raster - ' + nome, area_ha);
+}
+
+function calcularAreaVetor(fc, nome) {
+  var area_ha = fc.map(function(f) {
+    return f.set('area_ha', f.geometry().area(ee.ErrorMargin(1)).divide(10000));
+  });
+  var soma = area_ha.aggregate_sum('area_ha');
+  print('Vetor - ' + nome, soma);
+}
+
+calcularAreaRaster(smoothedCheia.eq(1), 'Agua');
+calcularAreaRaster(smoothedCheia.eq(0), 'Terra Firme');
+calcularAreaRaster(smoothedCheia.eq(2), 'Areas Umidas/Alagaveis');
+
+calcularAreaVetor(waterVector, 'Agua');
+calcularAreaVetor(landVector, 'Terra Firme');
+calcularAreaVetor(humidVector, 'Areas Umidas/Alagaveis');
+
+// 11. Visualização
+Map.centerObject(aoi, 10);
+
+Map.addLayer(dem, {min: 0, max: 300, palette: ['white', 'green', 'brown']}, 'DEM - GLO30', false);
+Map.addLayer(gedi, {min: 0, max: 50, palette: ['white', 'lime', 'darkgreen']}, 'GEDI Canopy Height', false);
+Map.addLayer(ndwiCheia, {min: -1, max: 1, palette: ['brown', 'white', 'blue']}, 'NDWI - Cheia', false);
+Map.addLayer(ndwiSeca, {min: -1, max: 1, palette: ['brown', 'white', 'blue']}, 'NDWI - Seca', false);
+Map.addLayer(compositeCheiaOptico.select(['B4','B3','B2']), {min: 0, max: 3000}, 'Sentinel-2 RGB - Cheia', false);
+Map.addLayer(compositeSecaOptico.select(['B4','B3','B2']), {min: 0, max: 3000}, 'Sentinel-2 RGB - Seca', false);
+Map.addLayer(compositeCheiaRadar.select('VH'), {min: -25, max: -5}, 'Radar VH - Cheia', false);
+Map.addLayer(compositeSecaRadar.select('VH'), {min: -25, max: -5}, 'Radar VH - Seca', false);
+
+Map.addLayer(smoothedCheia.updateMask(smoothedCheia.eq(1)), {palette: 'blue'}, 'Agua (Cheia - Suavizado)');
+Map.addLayer(smoothedCheia.updateMask(smoothedCheia.eq(2)), {palette: 'purple'}, 'Umido (Cheia - Suavizado)');
+Map.addLayer(smoothedCheia.updateMask(smoothedCheia.eq(0)), {palette: 'green'}, 'Terra (Cheia - Suavizado)');
+Map.addLayer(smoothedSeca.updateMask(smoothedSeca.eq(1)), {palette: 'cyan'}, 'Agua (Seca - Suavizado)');
+Map.addLayer(smoothedSeca.updateMask(smoothedSeca.eq(2)), {palette: 'magenta'}, 'Umido (Seca - Suavizado)');
+Map.addLayer(smoothedSeca.updateMask(smoothedSeca.eq(0)), {palette: 'olive'}, 'Terra (Seca - Suavizado)');
+Map.addLayer(waterVector, {color: 'blue'}, 'Poligono Agua');
+Map.addLayer(landVector, {color: 'green'}, 'Poligono Terra');
+Map.addLayer(humidVector, {color: 'purple'}, 'Poligono Areas Umidas');
